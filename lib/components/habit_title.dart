6859ed99558106e15c7baa30bf4ff455d87dd4b9
Danila Yevdokimov <danila.yevdokimov@gmail.com>
2024-06-15 00:11:35 +0300

    habit title widget added

---
+++ lib/components/habit_title.dart
@@ -0,0 +1,35 @@
+import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+
+class HabitTitle extends StatelessWidget {
+  HabitTitle(
+      {super.key,
+      required this.habitName,
+      required this.habitCompleted,
+      required this.onChanged});
+
+  final String habitName;
+  final bool habitCompleted;
+  final Function(bool?)? onChanged;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16),
+      child: Container(
+        padding: EdgeInsets.all(24),
+        decoration: BoxDecoration(
+          color: Colors.grey[100],
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: Row(
+          children: [
+            Checkbox(value: habitCompleted, onChanged: onChanged),
+            Text(habitName),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:habit_tracker/components/habit_title.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,14 +11,28 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePage extends State<HomePage> {
+  List todaysHabitList = [
+    ["Morning run", false],
+    ["Shower", false]
+  ];
+
+  void onTappedCheckBox(bool? value, int index) {
+    todaysHabitList[index][1] = value;
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[300],
-      body: ListView(
-        children: [
-          HabitTitle(),
-        ],
+      body: ListView.builder(
+        itemCount: todaysHabitList.length,
+        itemBuilder: (context, index) {
+          return HabitTitle(
+              habitName: todaysHabitList[index][0],
+              habitCompleted: todaysHabitList[index][1],
+              onChanged: (value) => onTappedCheckBox(value, index));
+        },
       ),
     );
   }
