f6637ff4c53e27e57ca901e62dd734e9ad136a44
Danila Yevdokimov <danila.yevdokimov@gmail.com>
2024-06-19 02:32:27 +0300

    loadData implemented

--- lib/data/habit_database.dart
+++ lib/data/habit_database.dart
@@ -1,4 +1,7 @@
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:habit_tracker/datetime/date_time.dart';
+
+final _habitDatabase = Hive.box("Habit_Database");
 
 class HabitDatabase {
   List todaysHabitList = [];
@@ -9,10 +12,23 @@ class HabitDatabase {
       ["Run", false],
       ["Smth", false]
     ];
+
+    _habitDatabase.put("START_DATE", todaysDateFormatted());
   }
 
   // загружаем
-  void loadData() {}
+  void loadData() {
+    if (_habitDatabase.get(todaysDateFormatted() == null)) {
+      todaysHabitList = _habitDatabase.get("CURRENT_HABBIT_LIST");
+
+      for (var i = 0; i < todaysHabitList.length; ++i) {
+        todaysHabitList[i][1] = false;
+      }
+    } else {
+      _habitDatabase.get(todaysDateFormatted());
+    }
+  }
+
   // обновляем
   void updateDatabase() {}
 }
--- lib/datetime/date_time.dart
+++ lib/datetime/date_time.dart
@@ -1,30 +1,23 @@
-// return todays date formatted as yyyymmdd
 String todaysDateFormatted() {
-  // today
   var dateTimeObject = DateTime.now();
 
-  // year in the format yyyy
   String year = dateTimeObject.year.toString();
 
-  // month in the format mm
   String month = dateTimeObject.month.toString();
   if (month.length == 1) {
     month = '0$month';
   }
 
-  // day in the format dd
   String day = dateTimeObject.day.toString();
   if (day.length == 1) {
     day = '0$day';
   }
 
-  // final format
   String yyyymmdd = year + month + day;
 
   return yyyymmdd;
 }
 
-// convert string yyyymmdd to DateTime object
 DateTime createDateTimeObject(String yyyymmdd) {
   int yyyy = int.parse(yyyymmdd.substring(0, 4));
   int mm = int.parse(yyyymmdd.substring(4, 6));
@@ -34,24 +27,19 @@ DateTime createDateTimeObject(String yyyymmdd) {
   return dateTimeObject;
 }
 
-// convert DateTime object to string yyyymmdd
 String convertDateTimeToString(DateTime dateTime) {
-  // year in the format yyyy
   String year = dateTime.year.toString();
 
-  // month in the format mm
   String month = dateTime.month.toString();
   if (month.length == 1) {
     month = '0$month';
   }
 
-  // day in the format dd
   String day = dateTime.day.toString();
   if (day.length == 1) {
     day = '0$day';
   }
 
-  // final format
   String yyyymmdd = year + month + day;
 
   return yyyymmdd;
